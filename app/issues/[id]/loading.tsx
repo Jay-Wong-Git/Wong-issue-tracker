8a4fa0573e34830ce9cc8926ee5bc085ccd4dfec
= <=>
2024-04-08 21:26:28 +0800

    Add issue detail skeleton & new issue skeleton

--- app/issues/[id]/loading.tsx
+++ app/issues/[id]/loading.tsx
@@ -1,7 +1,21 @@
+import { Box, Card, Flex, Heading } from "@radix-ui/themes";
 import React from "react";
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
 
 const LoadingIssueDetailPage = () => {
-  return <div>Loading...</div>;
+  return (
+    <Box>
+      <Skeleton width="6rem" />
+      <Flex gap="3" my="2">
+        <Skeleton width="3rem" />
+        <Skeleton width="8rem" />
+      </Flex>
+      <Card className="prose mt-5">
+        <Skeleton count={5} />
+      </Card>
+    </Box>
+  );
 };
 
 export default LoadingIssueDetailPage;
