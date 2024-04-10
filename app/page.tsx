65db1a2fa36f17d75afe12c4d20065266f9656fc
= <=>
2024-04-10 22:07:15 +0800

    Implement pagination

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,15 @@
+"use client";
+
+import { useSearchParams } from "next/navigation";
 import Pagination from "./components/Pagination";
 
 export default function Home() {
-  return <Pagination itemCount={100} pageSize={10} currentPage={10} />;
+  const searchParams = useSearchParams();
+  return (
+    <Pagination
+      itemCount={100}
+      pageSize={10}
+      currentPage={parseInt(searchParams.get("page") || "1")}
+    />
+  );
 }
