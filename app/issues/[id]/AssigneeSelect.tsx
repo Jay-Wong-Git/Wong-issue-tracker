d21d62ebb257515400978332892a354e19dd25f9
= <=>
2024-04-10 15:25:39 +0800

    Add toast notification

--- app/issues/[id]/AssigneeSelect.tsx
+++ app/issues/[id]/AssigneeSelect.tsx
@@ -2,8 +2,9 @@
 
 import { Issue, User } from "@prisma/client";
 import { Select } from "@radix-ui/themes";
-import { useMutation, useQuery } from "@tanstack/react-query";
+import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
+import toast, { Toaster } from "react-hot-toast";
 
 import Skeleton from "@/app/components/Skeleton";
 
@@ -27,27 +28,33 @@ const AssigneeSelect = ({
   if (isLoading) return <Skeleton />;
 
   return (
-    <Select.Root
-      onValueChange={(userId) =>
-        axios.patch(`/api/issues/${issueId}`, {
-          assignedToUserId: userId || null,
-        })
-      }
-      defaultValue={assignedToUserId ? assignedToUserId : ""}
-    >
-      <Select.Trigger placeholder="Assign..." />
-      <Select.Content>
-        <Select.Group>
-          <Select.Label>Suggestions</Select.Label>
-          <Select.Item value="">Unassigned</Select.Item>
-          {users?.map((user) => (
-            <Select.Item key={user.id} value={user.id}>
-              {user.name}
-            </Select.Item>
-          ))}
-        </Select.Group>
-      </Select.Content>
-    </Select.Root>
+    <>
+      <Select.Root
+        onValueChange={(userId) =>
+          axios
+            .patch(`/api/issues/${issueId}`, {
+              assignedToUserId: userId || null,
+            })
+            .then(() => toast.success("Changes saved successfully."))
+            .catch(() => toast.error("Changes could not be saved."))
+        }
+        defaultValue={assignedToUserId ? assignedToUserId : ""}
+      >
+        <Select.Trigger placeholder="Assign..." />
+        <Select.Content>
+          <Select.Group>
+            <Select.Label>Suggestions</Select.Label>
+            <Select.Item value="">Unassigned</Select.Item>
+            {users?.map((user) => (
+              <Select.Item key={user.id} value={user.id}>
+                {user.name}
+              </Select.Item>
+            ))}
+          </Select.Group>
+        </Select.Content>
+      </Select.Root>
+      <Toaster toastOptions={{ duration: 2000 }} />
+    </>
   );
 };
 
